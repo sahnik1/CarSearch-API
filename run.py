bfb60232728fb679c39eadbdd499d6062541b405
Karanjot Singh Sahni <sahnik@mcmaster.ca>
2019-11-17 14:39:04 -0500

    Simple API From DB Constructed

---
+++ run.py
@@ -0,0 +1,69 @@
+# Author: Karanjot Singh Sahni
+# Description: Web Api using Flask/Python for Aggregated Data from Previous CarSearch Project
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+import sqlite3
+import json
+
+app = Flask(__name__)
+api = Api(app)
+
+class IdSearch(Resource):
+    def get(self, carid):
+        con = sqlite3.connect("./data.db")
+        cur = con.cursor()
+        cur.execute("SELECT * FROM cars WHERE id=?;", [carid])
+        con.commit()
+        result = cur.fetchall()
+        if (result is None):
+            return { 'Status' : 'No Content' }, 204
+
+        desc = cur.description
+        column_names = [col[0] for col in desc]
+        data = [dict(zip(column_names, row)) for row in result]
+        datatemp = json.dumps(data)
+        datajson = json.loads(datatemp)
+
+        #jsontemp = {'ID' : result[0], 'Year' : str(result[1]), 'Make' :  str(result[3]), 'Model' :  str(result[4]), 'Category' :  str(result[2])}
+        return { 'Status' : 'Success', 'Data': datajson}, 200
+
+class YearList(Resource):
+    def get(self, year):
+        con = sqlite3.connect("./data.db")
+        cur = con.cursor()
+        cur.execute("SELECT * FROM cars WHERE year=?;", [year])
+        con.commit()
+        results = cur.fetchall()
+
+        desc = cur.description
+        column_names = [col[0] for col in desc]
+        data = [dict(zip(column_names, row)) for row in results]
+        datatemp = json.dumps(data)
+        datajson = json.loads(datatemp)
+
+        return { 'Status' : 'Success', 'Records_Found' : len(datajson), 'Data' : datajson }, 200
+
+class ComboList(Resource):
+    def get(self, year, make):
+        con = sqlite3.connect("./data.db")
+        cur = con.cursor()
+        cur.execute("SELECT * FROM cars WHERE year=:year AND make=:make;", {'year' : int(year), 'make' : make.upper()})
+        #con.commit()
+        results = cur.fetchall()
+
+        desc = cur.description
+        column_names = [col[0] for col in desc]
+        data = [dict(zip(column_names, row)) for row in results]
+        datatemp = json.dumps(data)
+        datajson = json.loads(datatemp)
+
+        return { 'Status' : 'Success', 'Year' : year, 'Make' : make, 'Records_Found' : len(datajson), 'Data' : datajson }, 200
+
+api.add_resource(IdSearch, '/cars/id=<int:carid>')
+api.add_resource(YearList, '/cars/year=<int:year>')
+api.add_resource(ComboList, '/cars/year=<int:year>,make=<string:make>')
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- store.py
+++ store.py
@@ -3,21 +3,31 @@ import sqlite3
 
 con = sqlite3.connect("./data.db")
 cur = con.cursor()
-#cur.execute("CREATE TABLE cars (id INTEGER PRIMARY KEY, year integer, category text, make text, model text);")
+cur.execute("CREATE TABLE cars (id INTEGER PRIMARY KEY, year integer, category text, make text, model text, No_Of_Recalls integer,Vehicles_Affected integer,City_MPG integer,Highway_MPG integer,Combined_MPG integer,Cylinders integer,Eng_Displ real,Transmission text,Drive text);")
 #id,Year,Category,Make,Model,No_Of_Recalls,Vehicles_Affected,City_MPG,Highway_MPG,Combined_MPG,Cylinders,Eng_Displ,Transmission,Drive
 
 id = 0
 dblister = []
+intkeys = ['Year', 'No_Of_Recalls','Vehicles_Affected','City_MPG','Highway_MPG','Combined_MPG','Cylinders']
+allkeys = ['id','Year','Category','Make','Model','No_Of_Recalls','Vehicles_Affected','City_MPG','Highway_MPG','Combined_MPG','Cylinders','Eng_Displ','Transmission','Drive']
 with open('./final.csv','rb') as filein:
     fileinfo = DictReader(filein)
     for row in fileinfo:
-        row['id'] = id
-        if (row['Year'] == 'NULL'):
-            dblister += [(row['id'], None, row['Category'], row['Make'], row['Model'])]
-        else:
-            dblister += [(row['id'], int(row['Year']), row['Category'], row['Make'], row['Model'])]
+        templist = []
+        for key in allkeys:
+            if (row[key] == 'NULL'):
+                templist += [0]
+            elif (key == 'id'):
+                templist += [id]
+            else:
+                if (key in intkeys):
+                    templist += [int(row[key])]
+                elif (key == 'Eng_Displ'):
+                    templist += [float(row[key])]
+                else:
+                    templist += [row[key].upper()]
+        dblister += [tuple(templist)]
         id += 1
-
-cur.execute("SELECT * FROM cars WHERE id = 0 ")
+#cur.execute("SELECT * FROM cars WHERE id = 0 ")
+cur.executemany("INSERT INTO cars (id, year, category, make, model,No_Of_Recalls,Vehicles_Affected,City_MPG,Highway_MPG,Combined_MPG,Cylinders,Eng_Displ,Transmission,Drive) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", dblister)
 con.commit()
-#cur.executemany("INSERT INTO cars (id, year, category, make, model) VALUES (?, ?, ?, ?, ?);", dblister)
